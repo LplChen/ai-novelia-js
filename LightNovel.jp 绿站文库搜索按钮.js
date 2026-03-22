c7c48ba5c2c928d4dc27cc16ebe22b75cac6ab64
LplChen <120585828+LplChen@users.noreply.github.com>
2026-03-22 10:21:30 +0800

    Rename script for clarity and accuracy

--- LightNovel.jp 绿站文库搜索按钮.js
+++ LightNovel.jp 绿站文库搜索按钮.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         LightNovel.jp搜索脚本
+// @name         LightNovel.jp 绿站文库搜索按钮
 // @namespace    http://tampermonkey.net/
 // @version      1.3
 // @description  在LightNovel.jp的书籍标题后增加去噪后的Novelia搜索按钮
